96c0e9074d739f1e1d9ade46ef86a6e772034ae5
Amber Mohler <ambermohler@iMac.local>
2020-02-25 15:12:14 -0500

    initial commit, did cra and added dependencies

---
+++ friends/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome!</h1>
+      <Link to='/login'>Login</Link>
+      
+     
+    </div>
+  );
+}
+
+export default App;
