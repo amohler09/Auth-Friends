1337a8728cff351bbbd3921b7070ef9ca219fb1e
Amber Mohler <ambermohler@iMac.local>
2020-02-25 15:27:26 -0500

    added links and routes and basic outlines of components to make sure everything is switching properly

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router-dom'
 
+import { Login } from './Components/Login'
+import { FriendsList } from './Components/FriendsList'
+
 
 import './App.css';
 
@@ -9,10 +12,20 @@ function App() {
     <div className="App">
       <h1>Welcome!</h1>
       <Link to='/login'>Login</Link>
+      <Link to='/protected'>Friends</Link>
       
+
+      <Switch>
+        <Route exact path='/protected' component={FriendsList} />
+        <Route path='/login' component={Login} />
+        <Route component={Login} />
+        
+      </Switch>
      
     </div>
   );
 }
 
 export default App;
+
+
---
+++ friends/src/Components/FriendsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const FriendsList = () => {
+    return (
+        <div>
+            Friends List
+        </div>
+    )
+}
